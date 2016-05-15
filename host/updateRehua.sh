a1a80399ee1ac0af6daeddc0397694b050023ce2
Nick Webster <nick@nick.geek.nz>
2016-05-15 16:35:20 +1200

    Saving one command

--- host/updateRehua.sh
+++ host/updateRehua.sh
@@ -6,4 +6,5 @@ HASH=`echo $HASH | awk -F" " '{print $(NF)}'`
 cd host
 echo $HASH > latest_hash.txt
 
+forever stopall
 echo "Restart rehua with forever to update. (The command to start is 'forever start -c python3 hostRehua.py')"
