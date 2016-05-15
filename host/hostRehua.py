1ed263bb9f14fb376b5d8764f5f79b642147fae8
Nick Webster <nick@nick.geek.nz>
2016-05-15 12:59:05 +1200

    Added hosting related scripts

---
+++ host/hostRehua.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+import os
+import time
+
+def getFile(path):
+	with open(path) as f:
+		return f.read()
+contents = getFile('latest_hash.txt').split('\n')[0]
+os.system('ipfs pin add -r {}'.format(contents))
+
+while 1:
+	time.sleep(600)
+	os.system('ipfs name publish {}'.format(contents))
---
+++ host/updateRehua.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd ..
+git pull origin master
+HASH=`ipfs add -r . | awk -F" " '{print $(NF-1)}'`
+HASH=`echo $HASH | awk -F" " '{print $(NF)}'`
+cd host
+echo $HASH > latest_hash.txt
+
+echo "Restart rehua with forever to update. (The command to start is 'forever start -c python3 hostRehua.py')"
