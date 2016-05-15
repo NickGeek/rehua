bed51410e485127b447540feab34e87d9810d1da
Nick Webster <nick@nick.geek.nz>
2016-05-15 13:48:18 +1200

    Cimproving the publishing

--- host/hostRehua.py
+++ host/hostRehua.py
@@ -7,6 +7,7 @@ def getFile(path):
 		return f.read()
 contents = getFile('latest_hash.txt').split('\n')[0]
 os.system('ipfs pin add -r {}'.format(contents))
+os.system('ipfs name publish {}'.format(contents))
 
 while 1:
 	time.sleep(600)
